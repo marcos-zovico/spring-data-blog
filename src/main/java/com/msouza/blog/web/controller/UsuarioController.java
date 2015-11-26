c9b4d30f2964068efe2c2c0ddfec3b2335eed45d
Marcos Antonio <marcosantoniozovico@hotmail.com>
2015-11-25 23:26:53 -0200

    Trabalhando em UsuarioController e adicionado taglibs

--- src/main/java/com/msouza/blog/web/controller/UsuarioController.java
+++ src/main/java/com/msouza/blog/web/controller/UsuarioController.java
@@ -1,8 +1,12 @@
 package com.msouza.blog.web.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +27,27 @@ public class UsuarioController {
 	
 	@Autowired
 	private AvatarService avatarService;
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView listUsuarios(ModelMap model){
+		
+		List<Usuario> usuarios = usuarioservice.findAll();
+		model.addAttribute("usuarios", usuarios);
+		return new ModelAndView("usuario/list", model);
+		
+	}
+	
+	
+	@RequestMapping(value = "/perfil/{id}", method = RequestMethod.GET)
+	public ModelAndView perfil(@PathVariable("id") Long id){
+		
+		ModelAndView view = new ModelAndView();
+		Usuario usuario = usuarioservice.findById(id);
+		view.addObject("usuario", usuario);
+		view.setViewName("usuario/perfil");
+		
+		return view;
+	}
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(@ModelAttribute("usuario") Usuario usuario,
