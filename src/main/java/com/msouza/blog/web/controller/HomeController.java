42d2df31369f4ec8ca719b002b28d811c8c2c0bd
Marcos Antonio <marcosantoniozovico@hotmail.com>
2015-12-21 00:18:06 -0200

    Criando a página principal do blog

---
+++ src/main/java/com/msouza/blog/web/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.msouza.blog.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.msouza.blog.entity.Postagem;
+import com.msouza.blog.service.PostagemService;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private PostagemService postagemService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home(ModelMap model) {
+		List<Postagem> postagens = postagemService.findAll();
+
+		model.addAttribute("postagens", postagens);
+
+		return new ModelAndView("posts", model);
+	}
+
+}
